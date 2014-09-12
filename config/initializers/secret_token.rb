7bb8b535808a0b86c43604c787b3030203ea89f4
rodrigotello <rodrigotello@outlook.com>
2014-09-12 09:44:17 -0500

    Second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vaesa::Application.config.secret_key_base = 'a8883cd418cdb23a97178627cedba6a7301f1cdf8b8c50cf4bb29d3b450b3dfbdd7c93fc59e8cc788453ffea6e6e0e63cafc9af350a038037e6f575cb9ab1625'
